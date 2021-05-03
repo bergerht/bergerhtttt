42534be07a0782ca3e0bfcc82b34c5d68cfafd74
wischn <annika@wischn.com>
2021-05-03 16:31:15 +0200

    never

--- src/gatsby-theme-docz/components/Kontakt/index.js
+++ src/gatsby-theme-docz/components/Kontakt/index.js
@@ -16,20 +16,19 @@ export const Kontakt = () => {
 				sx={{
 					alignItems: "center",
 					justifyContent: "center",
-					padding: "5% 0",
+					padding: "5% 3%",
 				}}
 			>
 				<Box
-					sx={{
-						alignItems: "center",
-						justifyContent: "center",
+					style={{
+						margin: "0% 2%",
 					}}
 				>
 					<Flex>
 						<a href='tel:+493438551287' className='x'>
 							<img src={telefon} alt='Telefonnummer' className='imgy' />
 						</a>
-						<div className='hovered'>
+						<div className='hovered l'>
 							<strong>Telefon:</strong> <br />
 							034385-51287
 						</div>
@@ -39,19 +38,14 @@ export const Kontakt = () => {
 						<a href='tel:+493438551504' className='x'>
 							<img src={handy} alt='Handynummer' className='imgy' />
 						</a>
-						<div className='hovered'>
+						<div className='hovered l'>
 							<strong>Handynummer:</strong> <br /> ???
 						</div>
 					</Flex>
 
-					<Flex
-						sx={{
-							alignItems: "center",
-							justifyContent: "center",
-						}}
-					>
+					<Flex>
 						<img src={post} alt='Postadresse' className='imgy' />
-						<div class='hovered'>
+						<div class='hovered l '>
 							<strong>Postadresse:</strong> <br />
 							Berger Haustechnik GmbH
 							<br />
@@ -64,11 +58,8 @@ export const Kontakt = () => {
 				</Box>
 
 				<Box
-					sx={{
-						alignItems: "center",
-						justifyContent: "center",
-						justifyItems: "center",
-						margin: "2%",
+					style={{
+						margin: "0% 2%",
 					}}
 				>
 					<div className='imgx'>
@@ -77,10 +68,8 @@ export const Kontakt = () => {
 				</Box>
 
 				<Box
-					sx={{
-						alignItems: "center",
-						justifyContent: "center",
-						justifyItems: "center",
+					style={{
+						margin: "0% 2%",
 					}}
 				>
 					{/* 3. Spalte */}
@@ -88,7 +77,7 @@ export const Kontakt = () => {
 						<a href='mailto:bht@bergerht.de' className='x'>
 							<img src={mail} alt='Mailadresse' className='imgy' />
 						</a>
-						<div class='hovered'>
+						<div class='hovered r'>
 							<strong>Mailadresse:</strong> <br />
 							bht@bergerht.de
 						</div>
@@ -98,14 +87,19 @@ export const Kontakt = () => {
 						<a href='fax:+493438551484' className='x'>
 							<img src={fax} alt='Faxadresse' className='imgy' />
 						</a>
-						<div class='hovered'>
+						<div class='hovered r'>
 							<strong>Fax:</strong> <br /> 034385-51484
 						</div>
 					</Flex>
 
-					<Flex>
+					<Flex
+						sx={{
+							alignItems: "center",
+							justifyContent: "center",
+						}}
+					>
 						<img src={adresse} alt='Adresse' className='imgy' />
-						<div class='hovered'>
+						<div class='hovered r'>
 							<strong>Adresse:</strong> <br />
 							Berger Haustechnik GmbH
 							<br />
