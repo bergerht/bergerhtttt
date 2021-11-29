32eb5a6fba51cf21fb67c05672183379efb92092
wischn <annika@wischn.com>
2021-11-29 10:31:19 +0100

    Impressum2

--- src/gatsby-theme-docz/components/NavGroup/index.js
+++ src/gatsby-theme-docz/components/NavGroup/index.js
@@ -1,49 +1,49 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { useEffect, useState, useRef } from 'react'
-import { useCurrentDoc } from 'docz'
+import { jsx } from "theme-ui";
+import { useEffect, useState, useRef } from "react";
+import { useCurrentDoc } from "docz";
 
-import * as styles from './styles'
-import { NavLink } from '../NavLink'
-import { ChevronDown } from '../Icons'
+import * as styles from "./styles";
+import { NavLink } from "../NavLink";
+import { ChevronDown } from "../Icons";
 
 export const NavGroup = ({ item, sidebarRef }) => {
-  const currentDoc = useCurrentDoc()
-  const currentDocRef = useRef()
-  const { name, menu, nr } = item
-  const [subheadingsVisible, setShowsubheadings] = useState(
-    currentDoc.menu === name
-  )
-  const toggleSubheadings = () => setShowsubheadings(!subheadingsVisible)
-  useEffect(() => {
-    if (sidebarRef.current && currentDocRef.current) {
-      sidebarRef.current.scrollTo(0, currentDocRef.current.offsetTop)
-    }
-  }, [])
-  return (
-    <div sx={styles.wrapper} data-testid="na v-group">
-      <div sx={styles.title} onClick={toggleSubheadings}>
-        {item.name}
-        <ChevronDown sx={styles.chevron({ active: subheadingsVisible })} />
-      </div>
-      <div sx={styles.sublinkWrapper} data-testid="nav-group-links">
-        {menu &&
-          subheadingsVisible &&
-          menu.map(menu => {
-            if (currentDoc.route === menu.route) {
-              return (
-                <NavLink key={menu.id} item={menu} ref={currentDocRef}>
-                  {menu.title}
-                </NavLink>
-              )
-            }
-            return (
-              <NavLink key={menu.id} item={menu}>
-                {menu.title}
-              </NavLink>
-            )
-          })}
-      </div>
-    </div>
-  )
-}
+	const currentDoc = useCurrentDoc();
+	const currentDocRef = useRef();
+	const { name, menu, nr } = item;
+	const [subheadingsVisible, setShowsubheadings] = useState(
+		currentDoc.menu === name
+	);
+	const toggleSubheadings = () => setShowsubheadings(!subheadingsVisible);
+	useEffect(() => {
+		if (sidebarRef.current && currentDocRef.current) {
+			sidebarRef.current.scrollTo(0, currentDocRef.current.offsetTop);
+		}
+	}, []);
+	return (
+		<div sx={styles.wrapper} data-testid='na v-group'>
+			<div sx={styles.title} onClick={toggleSubheadings}>
+				{item.name}
+				<ChevronDown sx={styles.chevron({ active: subheadingsVisible })} />
+			</div>
+			<div sx={styles.sublinkWrapper} data-testid='nav-group-links'>
+				{menu &&
+					subheadingsVisible &&
+					menu.map((menu) => {
+						if (currentDoc.route === menu.route) {
+							return (
+								<NavLink key={menu.id} item={menu} ref={currentDocRef}>
+									{menu.title}
+								</NavLink>
+							);
+						}
+						return (
+							<NavLink key={menu.id} item={menu}>
+								{menu.title}
+							</NavLink>
+						);
+					})}
+			</div>
+		</div>
+	);
+};
